046beae9b709323a905224a28917c3d333ce3f72
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 18:13:57 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -3,6 +3,7 @@ if [ `uname -m` = 'aarch64' ]; then
    wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
    chmod +x archiconda.sh
    bash archiconda.sh -b -p $HOME/miniconda
+   chmod -R 777 $HOME/miniconda/
    export PATH="$HOME/miniconda/bin:$PATH"
    hash -r
    chmod -R 777 $HOME/miniconda/
@@ -14,14 +15,14 @@ if [ `uname -m` = 'aarch64' ]; then
    sudo chmod -R 777 $HOME/miniconda/bin/activate 
    sudo chmod -R 777 /home/travis/.condarc
    conda info --envs
-   conda activate base
+   activate base
 else
    wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
    chmod +x miniconda.sh
    ./miniconda.sh -b-p $HOME/miniconda
    $HOME/miniconda/bin/conda init bash
    source ~/.bash_profile
-   conda activate base
+   activate base
 
 fi
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
