26d1b51e6aeec14f93cefb59f9ecbb34db890f84
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 15:01:50 +0530

    Update setup_dependencies_common.sh

--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -208,6 +208,7 @@ fi
 echo "appending pinned file "
 echo "conda ${CONDA_VERSION}" > $PIN_FILE_CONDA
 echo "conda install conda"
+sudo chmod -R 777 $MINICONDA_DIR
 retry_on_known_error conda install $QUIET conda
 echo "3"
 if [[ -z $CONDA_CHANNEL_PRIORITY ]]; then
