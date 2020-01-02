b43a0915f4146008883d86f87ddf53f78e4932fb
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 13:25:11 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -62,9 +62,7 @@ if [ `uname -m` = 'aarch64' ]; then
    $IS_SUDO chmod -R 777 $MINICONDA_DIR
    $IS_SUDO cp $MINICONDA_DIR/bin/* /usr/bin/
    $IS_SUDO rm /usr/bin/lsb_release
-   echo"echo /home/travis/archiconda3/etc/profile.d/conda.sh>> ~/.bashrc"
-   echo "/home/travis/archiconda3/etc/profile.d/conda.sh" >> ~/.bashrc
-   source ~/.bashrc
+   export PATH=$PATH:home/travis/archiconda3/bin/
    echo "conda activate base "
    sudo conda activate base 
 else
