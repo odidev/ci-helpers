cb431566e458b4a72b28d8fc25bf634aeb053544
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 19:18:29 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -9,7 +9,7 @@ if [ `uname -m` = 'aarch64' ]; then
    sudo conda config --set always_yes yes --set changeps1 no
    sudo conda update -q conda
    sudo conda info -a
-   conda activate base
+   sudo conda activate base
 else
    wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
    chmod +x miniconda.sh
