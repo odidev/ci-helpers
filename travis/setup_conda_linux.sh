639265fc5500bde6f1690c29dce0ad46528fd292
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 16:19:24 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -1,26 +1,26 @@
 
 if [ `uname -m` = 'aarch64' ]; then
-wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
-chmod +x archiconda.sh
-bash archiconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-sudo cp -r $HOME/miniconda/bin/* /usr/bin/
-hash -r
-sudo conda config --set always_yes yes --set changeps1 no
-sudo conda update -q conda
-sudo conda info -a
-export PKGS="numpy scipy coverage nose pip"
-if [ "$PANDAS_VERSION_STR" != "NONE" ]; then export PKGS="${PKGS} pandas${PANDAS_VERSION_STR}"; fi
-$HOME/miniconda/bin/conda init bash
-source ~/.bash_profile
-conda activate base
+   wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
+   chmod +x archiconda.sh
+   bash archiconda.sh -b -p $HOME/miniconda
+   export PATH="$HOME/miniconda/bin:$PATH"
+   sudo cp -r $HOME/miniconda/bin/* /usr/bin/
+   hash -r
+   sudo conda config --set always_yes yes --set changeps1 no
+   sudo conda update -q conda
+   sudo conda info -a
+   export PKGS="numpy scipy coverage nose pip"
+   if [ "$PANDAS_VERSION_STR" != "NONE" ]; then export PKGS="${PKGS} pandas${PANDAS_VERSION_STR}"; fi
+   $HOME/miniconda/bin/conda init bash
+   source ~/.bash_profile
+   conda activate base
 else
    wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
    chmod +x miniconda.sh
    ./miniconda.sh -b-p $HOME/miniconda
-    $HOME/miniconda/bin/conda init bash
-    source ~/.bash_profile
-    conda activate base
+   $HOME/miniconda/bin/conda init bash
+   source ~/.bash_profile
+   conda activate base
 
 fi
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
