a19f9101a37062c08ca309365d9d25e345946d7c
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-02 18:52:28 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -3,19 +3,12 @@ if [ `uname -m` = 'aarch64' ]; then
    wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
    chmod +x archiconda.sh
    bash archiconda.sh -b -p $HOME/miniconda
-   chmod -R 777 $HOME/miniconda/
    export PATH="$HOME/miniconda/bin:$PATH"
+   sudo cp -r $HOME/miniconda/bin/* /usr/bin/
    hash -r
-   chmod -R 777 $HOME/miniconda/
-   sudo chown 1000:1000 /home/travis/miniconda/pkgs/urls.txt
-   conda config --set always_yes yes --set changeps1 no
-   conda update -q conda
-   conda info -a
-   conda activate 
-   mkdir /home/travis/.condrac
-   sudo chmod -R 777 $HOME/miniconda/bin/activate 
-   sudo chmod -R 777 /home/travis/.condarc
-   conda info --envs
+   sudo conda config --set always_yes yes --set changeps1 no
+   sudo conda update -q conda
+   sudo conda info -a
    conda activate base
 else
    wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
