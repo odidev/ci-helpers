e2e2bd7c5ba00a79a0470291a29658184c0a22ec
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-03 16:35:25 +0530

    Update setup_dependencies_common.sh

--- travis/setup_dependencies_common.sh
+++ travis/setup_dependencies_common.sh
@@ -157,15 +157,27 @@ if [[ -z $PYTHON_VERSION ]]; then
 fi
 
 # We will use the 2.0.x releases as "stable" for Python 2.7 and 3.4
-if [[ $(python -c "from distutils.version import LooseVersion; import os;\
-        print(LooseVersion(os.environ['PYTHON_VERSION']) < '3.5')") == False ]]; then
-    export LATEST_ASTROPY_STABLE=4.0
-    export LATEST_NUMPY_STABLE=1.18
-else
-    export LATEST_ASTROPY_STABLE=2.0.16
-    export NO_PYTEST_ASTROPY=True
-    export LATEST_NUMPY_STABLE=1.16
-fi
+if [ `uname -m` = 'aarch64' ]; then
+
+    if [[ $(python -c "from distutils.version import LooseVersion; import os;\
+            print(LooseVersion(os.environ['PYTHON_VERSION']) < '3.7')") == False ]]; then
+        export LATEST_ASTROPY_STABLE=4.0
+        export LATEST_NUMPY_STABLE=1.18 
+    else
+        export LATEST_ASTROPY_STABLE=2.0.16
+        export NO_PYTEST_ASTROPY=True
+        export LATEST_NUMPY_STABLE=1.16
+    fi
+else 
+     if [[ $(python -c "from distutils.version import LooseVersion; import os;\
+            print(LooseVersion(os.environ['PYTHON_VERSION']) < '3.5')") == False ]]; then
+        export LATEST_ASTROPY_STABLE=4.0
+        export LATEST_NUMPY_STABLE=1.18 
+    else
+        export LATEST_ASTROPY_STABLE=2.0.16
+        export NO_PYTEST_ASTROPY=True
+        export LATEST_NUMPY_STABLE=1.16
+fi        
 export ASTROPY_LTS_VERSION=2.0.16
 export LATEST_SUNPY_STABLE=1.0.6
 
