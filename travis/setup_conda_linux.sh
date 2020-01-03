1be9b15e199bd6a1bb59192e13d18caa3557d752
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-03 13:08:08 +0530

    Update setup_conda_linux.sh

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -15,7 +15,7 @@ if [ `uname -m` = 'aarch64' ]; then
    if [[ -z $CONDA_ENVIRONMENT ]]; then
    sudo conda create $QUIET -n -b test $PYTHON_OPTION
    else
-   sudo conda create $QUIET -b -n test -f $CONDA_ENVIRONMENT
+   sudo conda create $QUIET -b -n test $CONDA_ENVIRONMENT
    fi
    sudo activate test
 else
